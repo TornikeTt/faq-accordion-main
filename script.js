16ae19c700f52a17b99f1f092a68e7e5643f19cc
Tornike <chargeishvilitornike@gmail.com>
2024-11-12 11:33:53 -0800

    change first ForLoop to ForEach function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let item = document.querySelectorAll(".faq-item");
+let items = document.querySelectorAll(".faq-item");
 
-for (let i = 0; i < item.length; i++) {
-    item[i].addEventListener("click", (e) => {
+items.forEach((item) => {
+    item.addEventListener("click", (e) => {
         let faq_question = document.querySelectorAll(".header-of-faqItem p");
         let faq_button = document.querySelectorAll(".header-of-faqItem img");
         let faq_answer = document.querySelectorAll(".faq-answer");
@@ -11,7 +11,7 @@ for (let i = 0; i < item.length; i++) {
                 for (let j = 0; j < faq_question.length; j++) {
                     if (j !== i) {
                         faq_answer[j].classList.remove("active");
-                        faq_button[j].src = "./assets/images/icon-plus.svg"; // Reset button to plus icon
+                        faq_button[j].src = "./assets/images/icon-plus.svg";
                     }
                 }
 
@@ -23,7 +23,7 @@ for (let i = 0; i < item.length; i++) {
             }
         }
     });
-}
+});
 
 /* 
 
